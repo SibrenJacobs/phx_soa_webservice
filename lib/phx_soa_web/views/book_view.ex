e29f225cf28179a35f4cbb584f185df7d5f00155
SibrenJacobs <sibren.jacobs@student.ucll.be>
2021-05-26 13:20:08 +0200

    changed json

--- lib/phx_soa_web/views/book_view.ex
+++ lib/phx_soa_web/views/book_view.ex
@@ -3,11 +3,11 @@ defmodule PhxSoaWeb.BookView do
   alias PhxSoaWeb.BookView
 
   def render("index.json", %{books: books}) do
-    %{data: render_many(books, BookView, "book.json")}
+    render_many(books, BookView, "book.json")
   end
 
   def render("show.json", %{book: book}) do
-    %{data: render_one(book, BookView, "book.json")}
+    render_one(book, BookView, "book.json")
   end
 
   def render("book.json", %{book: book}) do
